89822e63a70d6d20ee06950a06f28bc74519a4ac
= <saleh.majeed8@gmail.com>
2021-03-13 20:00:54 +0530

    through, transform and cocat with http

--- print.js
+++ print.js
@@ -1,2 +1,42 @@
 const fs = require('fs');
-fs.createReadStream(process.argv[2]).pipe(process.stdout);
+const through = require('through2');
+const transform = require('readable-stream').Transform;
+const concat = require('concat-stream');
+const http = require('http');
+const qs = require('querystring');
+
+const server = http.createServer(function (req, res) {
+	req.pipe(counter()).pipe(concat({ encoding: 'string' }, onbody));
+
+	function counter() {
+		let size = 0;
+		return through(function (buf, enc, next) {
+			size += buf.length;
+			if (size > 20) {
+				res.end('Boom!\n');
+			} else {
+				next(null, buf);
+			}
+		});
+	}
+
+	function onbody(body) {
+		const params = qs.parse(body);
+		console.log(params);
+		res.end('ok\n');
+	}
+});
+server.listen(5000);
+
+fs.createReadStream(process.argv[2]).pipe(through(toUpper)).pipe(process.stdout);
+fs.createReadStream(process.argv[2])
+	.pipe(
+		new transform({
+			transform: toUpper,
+		})
+	)
+	.pipe(process.stdout);
+
+function toUpper(buf, enc, next) {
+	next(null, buf.toString().toUpperCase());
+}
